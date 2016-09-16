08d986df81f7cbeee6b78392d37c25786b9531a9
pyeng <pyeng@users.noreply.github.com>
2016-09-16 11:35:32 +0300

    Added changes

--- cl1_ex3_strip.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import fileinput
-
-for line in fileinput.input():
-    line = line.strip()
-    print line.split(".")
-
-#echo '192.168.1.1'  | ./test3.py 
---
+++ cl2_ex1_binary_conv.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+ip_addr = raw_input("\nInput an IP address: ")
+
+octets = ip_addr.split(".")
+
+if len(octets) == 3:
+	octets.append("0")
+elif len(octets) == 4:
+	octets[3] = "0"
+
+bin_first = bin(int(octets[0]))
+hex_first = hex(int(octets[0]))
+net_num = ".".join(octets)
+
+print "\nIP network is %s" % net_num
+print "\n\n"
+print "%-20s %-20s %-20s" % ("NETWORK_NUMBER", "FIRST_OCTET_BINARY", "FIRST_OCTET_HEX")
+print "%-20s %-20s %-20s" % (net_num, bin_first, hex_first)
+print  "\n\n"
