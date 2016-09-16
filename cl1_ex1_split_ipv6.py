1922e600a076fa5cf2b821a0d70526d532b9d610
pyeng <pyeng@users.noreply.github.com>
2016-09-16 10:12:12 +0300

    Added changes

--- cl1_ex1_split_ipv6.py
+++ cl1_ex1_split_ipv6.py
@@ -4,4 +4,13 @@ ipv6_addr = "FE80:0000:0000:0000:0101:A3EF:EE1E:1719"
 
 ipv6_split = ipv6_addr.split(":")
 
+print 
+print "IPv6 address split:"
 print ipv6_split
+print
+
+ipv6_new = ":".join(ipv6_split)
+
+print "IPv6 address re-joined:"
+print ipv6_new
+print
