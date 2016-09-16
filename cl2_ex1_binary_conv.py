8a5c8a7914188a60e94004500be25b1426acdaf3
pyeng <pyeng@users.noreply.github.com>
2016-09-16 11:40:30 +0300

    Added changes

--- cl2_ex1_binary_conv.py
+++ cl2_ex1_binary_conv.py
@@ -13,7 +13,7 @@ bin_first = bin(int(octets[0]))
 hex_first = hex(int(octets[0]))
 net_num = ".".join(octets)
 
-print "\nIP network is %s" % net_num
+print "\nYour IP network is %s" % net_num
 print "\n\n"
 print "%-20s %-20s %-20s" % ("NETWORK_NUMBER", "FIRST_OCTET_BINARY", "FIRST_OCTET_HEX")
 print "%-20s %-20s %-20s" % (net_num, bin_first, hex_first)
