4e6642d23bf2ec41a5674395c522daeedec41903
pyeng <pyeng@users.noreply.github.com>
2016-09-19 19:50:40 +0300

    Added changes

--- cl3_ex3_sh_ip_int_br.py
+++ cl3_ex3_sh_ip_int_br.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import pprint
+
 show_ip_int_brief = '''
 Interface            IP-Address      OK?     Method      Status     Protocol
 FastEthernet0   unassigned      YES     unset          up          up
@@ -15,4 +17,24 @@ Vlan10              10.220.88.1     YES     NVRAM       up          up
 Vlan20              192.168.0.1     YES     NVRAM       down      down
 Vlan100            10.220.84.1     YES     NVRAM       up          up
 '''
-print show_ip_int_brief.strip("\n")
+
+show_ip_list = []
+
+show_ip_int_brief = (show_ip_int_brief.strip("\n")).split("\n")
+
+for i in show_ip_int_brief:
+	
+	if "Interface" in i:
+		continue
+	i = i.split()
+	if len(i) == 6:
+	
+		(iface, ip_addr, ok, method, status, protocol) = i
+		
+		if (status == "up") and (protocol == "up"):
+			show_ip_list.append((iface, ip_addr, status, protocol))
+
+print "\n"
+pprint.pprint(show_ip_list)
+#print show_ip_list
+print "\n"
