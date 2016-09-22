fdc89c7e618c469aec81a0817aa24bf85ed9a40c
pyeng <pyeng@users.noreply.github.com>
2016-09-22 23:52:06 +0300

    added changes to cl5_ex1

--- cl5_ex1_cdp_parse.py
+++ cl5_ex1_cdp_parse.py
@@ -2,12 +2,14 @@
 
 import pprint
 
-from cdp_data import (sw1_show_cdp_neighbors, sw1_show_cdp_neighbors_detail, \
-r1_show_cdp_neighbors, r1_show_cdp_neighbors_detail, \
-r2_show_cdp_neighbors, r2_show_cdp_neighbors_detail, \
-r3_show_cdp_neighbors, r3_show_cdp_neighbors_detail, \
-r4_show_cdp_neighbors, r4_show_cdp_neighbors_detail, \
-r5_show_cdp_neighbors, r5_show_cdp_neighbors_detail)
+from cdp_data import (
+	sw1_show_cdp_neighbors, sw1_show_cdp_neighbors_detail, 
+	r1_show_cdp_neighbors, r1_show_cdp_neighbors_detail, 
+	r2_show_cdp_neighbors, r2_show_cdp_neighbors_detail, 
+	r3_show_cdp_neighbors, r3_show_cdp_neighbors_detail, 
+	r4_show_cdp_neighbors, r4_show_cdp_neighbors_detail, 
+	r5_show_cdp_neighbors, r5_show_cdp_neighbors_detail
+	)
 
 cdp_neighbors = (
     sw1_show_cdp_neighbors_detail, 
@@ -22,25 +24,43 @@ cdp_neighbors = (
 network_devices = {}
 
 for cdp_ent in cdp_neighbors:
+
 	cdp_ent = cdp_ent.strip("\n").split("\n")
+
 	for i in cdp_ent:
+	
 		if "Device ID: " in i:
-			hostname = i.split("Device ID: ")[1]
-			network_devices[hostname] = {}
+			#hostname = i.split("Device ID: ")[1]
+			#network_devices[hostname] = {}
+			(junk, hostname) = i.split('Device ID: ')
+			hostname = hostname.strip()
+
+			if not hostname in network_devices:
+				network_devices[hostname] = {}
+	
 		if "IP address: " in i:
 			ip_addr = i.split("IP address: ")[1]
-			network_devices[hostname]["ip"] = ip_addr
+			if hostname:
+				network_devices[hostname]["ip"] = ip_addr
+	
 		if "Platform: " in i:
 			model = (i.split(",")[0]).split(" ")[-1]
-			network_devices[hostname]["model"] = model
 			vendor = (i.split(",")[0]).split(" ")[-2]
-			network_devices[hostname]["vendor"] = vendor
+				
 			if "Router" in i:
 				device_type = "router"
+	
 			elif "Switch" in i:
 				device_type = "switch"
+	
 			else:
 				device_type = "unknown"
-			network_devices[hostname]["device_type"] = device_type
+			
+			if hostname:
+				network_devices[hostname]["model"] = model
+				network_devices[hostname]["vendor"] = vendor
+				network_devices[hostname]["device_type"] = device_type
 
+print '\n'
 pprint.pprint(network_devices)
+print '\n'
