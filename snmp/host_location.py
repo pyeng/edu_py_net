7c9bedbbfd643a155ca9ba4747f1f8a1e05e0345
pyeng <pyeng@users.noreply.github.com>
2016-09-24 14:09:08 +0300

    edit host_location.py

--- snmp/host_location.py
+++ snmp/host_location.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python
 
+import sys
+
 from snmp_helper import snmp_get_oid,snmp_extract
 
-switch = [192.168.12.1,	192.168.12.90, 192.168.12.42, 192.168.12.101]
+if len(sys.argv) != 2:
+	#Exit script
+	sys.exit("Usage: ./host_location.py <SNMP Community>")
 
-COMMUNITY_STRING = 'SnNeMtP'
+switch = (
+    "192.168.12.1",
+    "192.168.12.2",
+    "192.168.12.3",
+    "192.168.12.4",
+    "192.168.12.5",
+ 	)
+
+COMMUNITY_STRING = sys.argv.pop()
 SNMP_PORT = 161
 device = (switch, COMMUNITY_STRING, SNMP_PORT)
 
 for i in switch:
-	snmp_data = snmp_get_oid(device, oid='1.3.6.1.2.1.1.5.0', display_errors=True)
+	device = (i, COMMUNITY_STRING, SNMP_PORT)
+	snmp_hostname = snmp_get_oid(device, oid='1.3.6.1.2.1.1.5.0', display_errors=True)
+	snmp_location = snmp_get_oid(device, oid='1.3.6.1.2.1.1.6.0', display_errors=True)
+	hostname = snmp_extract(snmp_hostname)
+	location = snmp_extract(snmp_location)
 
-	output = snmp_extract(snmp_data)
-
-print "%s, %s" % (switch, output)
+	print "%-15s %-8s %-8s" % (i, hostname, location)
