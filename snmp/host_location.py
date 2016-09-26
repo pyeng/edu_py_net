16cbc3f43358d8c7f9b2daee37aa9dac2f047f10
pyeng <pyeng@users.noreply.github.com>
2016-09-26 15:56:23 +0300

    add changes snmp script

--- snmp/host_location.py
+++ snmp/host_location.py
@@ -12,9 +12,7 @@ switch = (
     "192.168.12.1",
     "192.168.12.2",
     "192.168.12.3",
-    "192.168.12.4",
-    "192.168.12.5",
- 	)
+    )
 
 COMMUNITY_STRING = sys.argv.pop()
 SNMP_PORT = 161
@@ -24,7 +22,7 @@ for i in switch:
 	device = (i, COMMUNITY_STRING, SNMP_PORT)
 	snmp_hostname = snmp_get_oid(device, oid='1.3.6.1.2.1.1.5.0', display_errors=True)
 	snmp_location = snmp_get_oid(device, oid='1.3.6.1.2.1.1.6.0', display_errors=True)
-	hostname = snmp_extract(snmp_hostname)
+	hostname = snmp_extract(snmp_hostname).strip()
 	location = snmp_extract(snmp_location)
 
 	print "%-15s %-8s %-8s" % (i, hostname, location)
