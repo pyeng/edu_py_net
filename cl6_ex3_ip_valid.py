c2e5b2de9ad87a4b568a3325c630001a5ff09a28
pyeng <pyeng@users.noreply.github.com>
2016-10-31 08:20:58 +0200

    changes cl6_ex3_ip_valid.py

--- cl6_ex3_ip_valid.py
+++ cl6_ex3_ip_valid.py
@@ -12,17 +12,7 @@ output.
 interpreter shell and test it (use both 'import x' and 
 'from x import y').
 """
-"""
-ip_address = "10.100.0.1"
-ip_split = ip_address.split(".")
-first_octet = ip_split[0]
 
-if len(ip_split) == 4:
-	if 1 < int(first_octet) < 223:
-		if int(first_octet) != 127:
-			if ip_split[0:2] != ["169", "254"]:
-				for octet in ip_split[1:4]
-"""
 def ip_valid(ip_address):
 
 	 # Make sure IP has four octets
