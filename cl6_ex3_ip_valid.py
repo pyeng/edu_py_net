95b40c9526ee3f46f0a59850edf4c998bcea84df
pyeng <pyeng@users.noreply.github.com>
2016-10-31 08:20:16 +0200

    add changes cl6_ex3_ip_valid.py

--- cl6_ex3_ip_valid.py
+++ cl6_ex3_ip_valid.py
@@ -12,7 +12,7 @@ output.
 interpreter shell and test it (use both 'import x' and 
 'from x import y').
 """
-
+"""
 ip_address = "10.100.0.1"
 ip_split = ip_address.split(".")
 first_octet = ip_split[0]
@@ -22,4 +22,42 @@ if len(ip_split) == 4:
 		if int(first_octet) != 127:
 			if ip_split[0:2] != ["169", "254"]:
 				for octet in ip_split[1:4]
+"""
+def ip_valid(ip_address):
+
+	 # Make sure IP has four octets
+	 octets = ip_address.split(".")
+	 if len(octets) != 4:
+	 	return False
+
+	 # convert octet from string to int
+	 for i, octet in enumerate(octets):
+	 	try:
+	 		octets[i] = int(octet)
+	 	except ValueError:
+	 		# couldn't convert octet to an integer
+	 		return False
+
+	 # map variables to elements of octets list
+	 first_octet, second_octet, third_octet, fourth_octet = octets
+
+	 # Check first_octet meets conditions
+	 if first_octet < 1:
+	 	return False
+	 elif first_octet > 223:
+	 	return False
+	 elif first_octet == 127:
+	 	return False
+
+	 # Check 169.254.X.X condition
+	 if first_octet == 169 and second_octet == 254:
+	 	return False
+
+	 # Check 2nd - 4th octets
+	 for octet in (second_octet, third_octet, fourth_octet):
+	 	if (octet < 0) or (octet > 255):
+	 		return False
+
+	 # Passed all of the checks
+	 return True
 
