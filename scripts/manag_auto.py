2d25c797ea72234c5577c746a4f949d04b543471
pyeng <pyeng@users.noreply.github.com>
2016-11-14 10:44:15 +0200

    add changes to manag_auto

--- scripts/manag_auto.py
+++ scripts/manag_auto.py
@@ -4,36 +4,39 @@ import os
 import multiprocessing
 import subprocess
 import ipaddress
+import pprint
 import re
 from snmp_helper import snmp_get_oid,snmp_extract
 
-manag_net = ipaddress.ip_network(u"192.168.21.0/26")
+manag_net = ipaddress.ip_network(u"10.90.90.0/24")
+
+oid_model = ".1.3.6.1.2.1.1.1.0"
+oid_hostname = ".1.3.6.1.2.1.1.5.0"
+oid_location = ".1.3.6.1.2.1.1.6.0"
 
 DNULL = open(os.devnull, 'w')
 
-def data_from_snmp(switch_ip):
+
+def data_from_snmp(switch_ip, oid):
     COMMUNITY_STRING = 'SnNeMtP'
     SNMP_PORT = 161
-    used_ports = 0
-
-    oid_model = ".1.3.6.1.2.1.1.1.0"
-    oid_hostname = ".1.3.6.1.2.1.1.5.0"
-    oid_location = ".1.3.6.1.2.1.1.6.0"
 
     switch = (switch_ip, COMMUNITY_STRING, SNMP_PORT)
-    snmp_data = snmp_get_oid(switch, oid_model, display_errors=True)
-    model = snmp_extract(snmp_data)
-    return model
+    snmp_data = snmp_get_oid(switch, oid, display_errors=True)
+    output = snmp_extract(snmp_data).split()[:2]
+    return output
 
 
-def ping(host,mp_queue):
+def action(host,mp_queue):
     response = subprocess.call(["ping", "-c", "2", "-w", '2', host], stdout=DNULL)
     if response == 0:
-    	dev = data_from_snmp(host)
-        print host, dev
+    	hostname = data_from_snmp(host, oid_hostname)
+        location = data_from_snmp(host, oid_location)
+        vendor = data_from_snmp(host, oid_model)
+        print host, hostname, location, vendor
         result = True
     else:
-        print host, 'is down!'
+        #print host, 'is down!'
         result = False
     mp_queue.put((result,host))
 
@@ -44,9 +47,9 @@ def worker(manag_net):
     	device = str(device)
     	gw = re.search(r"(\.1)$", device)
         if gw:
-            print "gw"
+            print 
         else:
-            p = multiprocessing.Process(target=ping, args=(device, mp_queue))
+            p = multiprocessing.Process(target=action, args=(device, mp_queue))
             processes.append(p)
             p.start()
     for p in processes:
